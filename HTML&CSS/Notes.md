7e32a3708a1de8547d80d4fb88f59c1aa89c19f5
Divyanshu81 <divyanshu52singhal@gmail.com>
2024-06-04 12:27:52 +0530

    working on blogging and updated some notes

--- HTML&CSS/Notes.md
+++ HTML&CSS/Notes.md
@@ -1,2 +1,11 @@
-# In Html tags can have multiple classes which can be used like 
+### In Html tags can have multiple classes which can be used like 
 <[tag_name] class ="class-name1 class-name2 class-name3">
+
+### inhert properties in css
+sample :
+p{
+    color : red;
+}
+a{
+    color : inherit;
+}
--- InsidePython/functoolanddecorator.py
+++ InsidePython/functoolanddecorator.py
@@ -5,14 +5,14 @@ user1 = {"username":"div","access_level":"admin"}
 
 def make_secure(access_level):
     def decorator(func): #func is built in 
-        @functools.wraps(func)  # making functional decorator
+        @functools.wraps(func)  # making function decorator
         def secure_function(*args,**kwargs):
             if user1["access_level"]==access_level: #checking global user not passed
                 return func(*args,**kwargs)
             else:
                 return f"No {access_level} permission for {user['username']}."
         return secure_function
-    return decorator
+    return decorator #return to function call
 
 @make_secure("admin")   #access_level argument
 def get_admin_password():
